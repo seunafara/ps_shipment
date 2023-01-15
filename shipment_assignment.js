856e320ff40efdffdd39056893eafbd672d3ea49
Ola <seunvigo@gmail.com>
2023-01-15 12:55:29 -0800

    (chore): add func comments

--- shipment_assignment.js
+++ shipment_assignment.js
@@ -1,5 +1,13 @@
 const fs = require("fs");
 
+/**
+
+@params {string} destination - The shipment destination street name
+@params {string} driver - The driver name
+@desc - Calculates the base suitability score based on the length of the shipment destination street name, the number of vowels and consonants in the driver name.
+@returns {number} The base suitability score
+*/
+
 const getBaseScore = (destination, driver) => {
   const vowels = "aeiou";
   const driverName = driver.toLowerCase();
@@ -15,6 +23,13 @@ const getBaseScore = (destination, driver) => {
   }
 };
 
+/**
+
+@params {string} destination - The shipment destination street name
+@params {string} driver - The driver name
+@desc - Increases the base suitability score if the length of the shipment destination street name shares any common factors with the length of the driver name
+@returns {number} The suitability score
+*/
 const getScore = (destination, driver) => {
   const baseScore = getBaseScore(destination, driver);
   if (destination.length % driver.length === 0) {
@@ -23,6 +38,13 @@ const getScore = (destination, driver) => {
   return baseScore;
 };
 
+/**
+
+@params {string[]} destinations - An array of shipment destination street names
+@params {string[]} drivers - An array of driver names
+@desc - Matches shipment destinations with drivers in a way that maximizes the total suitability score over the set of drivers.
+@returns {object} An object containing total suitability score and shipment destination - driver matches
+*/
 const assignShipments = (destinations, drivers) => {
   let totalScore = 0;
   const matches = {};
@@ -56,6 +78,10 @@ const destinationsFile = process.argv[2];
 // Grab [drivers_file] from CLI args
 const driversFile = process.argv[3];
 
+/**
+
+@desc - Runs the program, reads the input files, calls the assignShipments function and prints the output
+*/
 const run = () => fs.readFile(destinationsFile, "utf-8", (err, destinations) => {
   if (err) throw err;
 
